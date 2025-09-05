03d5d6a138d1e2bc92ae24166044ca8824ae8ba5
Mike Slinn <mslinn@mslinn.com>
2025-09-05 17:28:23 -0400

    -

--- lib/acb_class.rb
+++ lib/acb_class.rb
@@ -2,32 +2,34 @@ require 'erb'
 
 class AmbiguousMethodError < StandardError; end
 
-# This class must be constructed *after* the objects and modules to be referenced
-# from ERBs have been constructed and are available.
+# This class must be constructed *after* the binding, the objects, and the modules to be referenced
+# have been constructed and are available.
 #
-# Provide a binding that resolves methods against an array of objects,
-# but raises NameError if more than one object responds to the same method.
-# Only public methods will be found.
+# Provide a binding that resolves methods against an array of objects.
+# Raise NameError if more than one object responds to the same method.
+# Only public methods will be honored.
 #
 # Modules and classes can also contribute methods to delegation resolution.
 #
-# An ERB can use:
+# The internally constructed ERB provided by ArbitraryContextBinding#render can use:
 #  - Instance vars: <%= @repository.user_name %>
 #  - Delegated instance methods: <%= user_name %>
 #  - Module and class methods: <%= Project.version %>
 #  - Delegated module methods: <%= version %>
 #
-# ERB trim mode is set to '-'. See https://www.rubydoc.info/stdlib/erb/ERB#initialize-instance_method
+# ERB trim mode is set to '-'.
+# See https://www.rubydoc.info/stdlib/erb/ERB#initialize-instance_method
 #
-# Unlike an approach that uses method_missing, this delegation approach invokes real methods created with
-# define_singleton_method. This means the methods can be used with respond_to? and the code runs much faster.
+# Unlike an approach that uses method_missing, the delegation approach used invokes real singleton methods
+# created with define_singleton_method. This means the methods can be used with respond_to?
+# and the code runs much faster.
 #
 # Ambiguous method references return true in response to respond_to?, but raise NameError when invoked.
+# See the RSpec tests for further information.
 #
 # @example
 # acb = ArbitraryContextBinding.new(objects: [obj1, obj2])
 # expanded_template = acb.render template
-#
 module ArbitraryContextBinding
   class ArbitraryContextBinding
     attr_reader :base_binding, :modules, :objects
@@ -43,8 +45,8 @@ module ArbitraryContextBinding
       raise ArgumentError, 'objects must be an Array' unless objects.is_a? Array
       raise ArgumentError, 'modules must be an Array' unless modules.is_a? Array
 
-      @objects = objects.dup
-      @modules = modules.dup
+      @objects = objects.dup # shallow copy
+      @modules = modules.dup # shallow copy
       @base_binding = base_binding
       define_module_constants!
       define_delegators!
@@ -62,8 +64,8 @@ module ArbitraryContextBinding
       #  - no following spaces are removed
       #  - only a single newline is removed
       erb = ERB.new template, trim_mode: '-'
-      ctx = ArbitraryContextBinding.new(base_binding: @base_binding, modules: @modules, objects: @objects)
-      erb.result ctx.the_binding
+      acb = ArbitraryContextBinding.new(base_binding: @base_binding, modules: @modules, objects: @objects)
+      erb.result acb.the_binding
     end
 
     def to_string
@@ -76,7 +78,7 @@ module ArbitraryContextBinding
     end
 
     # Order is important:
-    alias inspect_original inspect
+    alias inspect_original inspect # Call this if you need to
     alias inspect to_string
     alias to_s to_string
 
@@ -111,19 +113,22 @@ module ArbitraryContextBinding
       end
 
       # Copy delegators into this instance and ensure only one method per name is defined
+      #
+      # define_singleton_method defines a method that only one object can use.
+      # It attaches the method to that object’s singleton class (a.k.a its eigenclass).
       method_map.each do |method_name, responders|
         case responders.size
         when 0 # This should not be possible
           # Do nothing because respond_to? will return false and a NameError will be raised if invoked as usual
         when 1 # Happy path: exactly one responder
           target = responders.first
-          define_singleton_method(method_name) do |*args, &block|
+          define_singleton_method(method_name) do |*args, &block| # add this method as method_name to self
             target.public_send(method_name, *args, &block)
           end
         else # Error: more than one responder (ambiguous)
           signatures = responders.map(&:to_s).join(', ')
           error_message = "Ambiguous method '#{method_name}': multiple objects/modules (#{signatures}) respond"
-          define_singleton_method(method_name) do |*|
+          define_singleton_method(method_name) do |*| # add this method as method_name to self
             raise AmbiguousMethodError, error_message
           end
         end
