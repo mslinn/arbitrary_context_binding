d6bf66ef39a964cbb660525922c9b5bf1161e1b9
Mike Slinn <mslinn@mslinn.com>
2025-09-15 15:00:44 -0400

    -

--- playground/mirrored_instance_binding_example.rb
+++ playground/mirrored_instance_binding_example.rb
@@ -7,14 +7,14 @@ require_relative '../lib/custom_binding'
 # This code excersizes most methods in CustomBinding anyway.
 
 # Test data
-class MyClass
+class Foo
   def initialize
     @foo = 42
   end
 end
 
 # Another class to demonstrate adding an instance to the mirrored binding
-class AnotherClass
+class Bar
   attr_accessor :bar
 
   def initialize(bar)
@@ -22,49 +22,57 @@ class AnotherClass
   end
 end
 
+# Excercise all methods
 def full_test
-  custom_binding = CustomBinding.new MyClass.new
+  puts 'full_test'
+  custom_binding = CustomBinding.new Foo.new
 
   # Create a new (internal) binding for the same object
-  new_binding = custom_binding.mirror_binding
-  # Both original_binding and new_binding are for the same object (obj).
+  mirrored_binding = custom_binding.mirror_binding
+  # Both original_binding and mirrored_binding are for the same object (obj).
   # Any changes to @foo via either binding are reflected in the other,
   # because they reference the same object’s instance variable.
 
   # Set or update the instance variable via the original binding
   custom_binding.eval '@foo = 100'
-  puts new_binding.eval '@foo' # => 100
+  puts custom_binding.eval '@foo' # => 100
+  puts mirrored_binding.eval '@foo' # => 100
 
   # Update the reference in the original binding via the new binding
-  new_binding.eval '@foo = 200'
+  mirrored_binding.eval '@foo = 200'
   puts custom_binding.eval '@foo' # => 200
+  puts mirrored_binding.eval '@foo' # => 200
 
-  # Add a new instance of AnotherClass to the mirrored binding
-  another_obj = AnotherClass.new 'value of another_obj.bar'
-  custom_binding.add_object_to_binding_as('@another', another_obj)
-  puts custom_binding.eval '@another.bar' # => 'value of another_obj.bar'
+  # Add a new instance of Bar to the mirrored binding
+  bar = Bar.new 'value of bar.bar'
+  custom_binding.add_object_to_binding_as('@another', bar)
+  puts custom_binding.eval '@another.bar' # => 'value of bar.bar'
+  puts mirrored_binding.eval '@another.bar' # => 'value of bar.bar'
 
   # Change the value via the original binding
-  custom_binding.eval '@another.bar = "value of another_obj.bar changed"'
-  puts new_binding.eval '@another.bar' # => 'value of another_obj.bar changed'
+  custom_binding.eval '@another.bar = "value of bar.bar changed"'
+  puts mirrored_binding.eval '@another.bar' # => 'value of bar.bar changed'
 
   # Ensure this still works:
   puts custom_binding.eval '@foo' # => 200
-  puts new_binding.eval '@foo' # => 200
+  puts mirrored_binding.eval '@foo' # => 200
 end
 
+# Just excercise those methods required by Nugem
 def nugem_test
+  puts 'nugem_test'
   custom_binding = CustomBinding.new TOPLEVEL_BINDING
 
-  obj = AnotherClass.new 'value of obj.bar'
-  custom_binding.add_object_to_binding_as('@obj', obj)
+  bar = Bar.new 'value of bar.bar'
+  custom_binding.add_object_to_binding_as('@test_bar', bar)
 
-  another_obj = AnotherClass.new 'value of another_obj.bar'
-  custom_binding.add_object_to_binding_as('@another', another_obj)
+  another_bar = Bar.new 'value of another_bar.bar'
+  custom_binding.add_object_to_binding_as('@another_test_bar', another_bar)
 
-  puts custom_binding.eval '@foo' # => 200
-  # puts new_binding.eval '@another.foo' # => 200
+  puts custom_binding.eval '@test_bar.bar' # => "value of bar.bar"
+  puts custom_binding.eval '@another_test_bar.bar' # => "value of another_bar.bar"
 end
 
 nugem_test
-# full_test
+puts
+full_test
