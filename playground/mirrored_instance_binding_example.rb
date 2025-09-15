78bde61f14ddda7a26f44169cf220a06e1c53b41
Mike Slinn <mslinn@micronauticsresearch.com>
2025-09-15 11:01:41 -0400

    -

--- playground/mirrored_instance_binding_example.rb
+++ playground/mirrored_instance_binding_example.rb
@@ -7,10 +7,6 @@ class MyClass
   def initialize
     @foo = 42
   end
-
-  def get_binding
-    binding
-  end
 end
 
 # Another class to demonstrate adding an instance to the mirrored binding
@@ -24,27 +20,31 @@ end
 
 # Get to work
 obj = MyClass.new
-original_binding = obj.get_binding
+original_binding = obj.instance_eval { binding } # this is how to get the binding for any object
 
 # Create a new binding for the same object
 new_binding = CustomBinding.mirrored_instance_binding(original_binding)
+# Both original_binding and new_binding are for the same object (obj).
+# Any changes to @foo via either binding are reflected in the other,
+# because they reference the same object’s instance variable.
 
 # Set or update the instance variable via the original binding
-original_binding.eval('@foo = 100')
-puts new_binding.eval('@foo') # => 100
+original_binding.eval '@foo = 100'
+puts new_binding.eval '@foo' # => 100
 
-# Update via the new binding
-new_binding.eval('@foo = 200')
-puts original_binding.eval('@foo') # => 200
+# Update the reference in the original binding via the new binding
+new_binding.eval '@foo = 200'
+puts original_binding.eval '@foo' # => 200
 
 # Add a new instance of AnotherClass to the mirrored binding
-another_obj = AnotherClass.new('baz')
+another_obj = AnotherClass.new 'value of another_obj.bar'
 # ObjectSpace._id2ref retrieves the object by its id, which is globally available.
-new_binding.eval("@another = ObjectSpace._id2ref(#{another_obj.object_id})")
-puts original_binding.eval('@another.bar') # => 'baz'
+new_binding.eval "@another = ObjectSpace._id2ref(#{another_obj.object_id})"
+puts original_binding.eval '@another.bar' # => 'value of another_obj.bar'
 
 # Change the value via the original binding
-original_binding.eval('@another.bar = "changed"')
-puts new_binding.eval('@another.bar') # => 'changed'
+original_binding.eval '@another.bar = "changed"'
+puts new_binding.eval '@another.bar' # => 'changed'
 
-puts original_binding.eval('@foo') # => 200
+# Ensure this still works:
+puts original_binding.eval '@foo' # => 200
